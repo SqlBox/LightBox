540c25c11d978196df721345c770ec1a30432298
avraampiperidis <piperidis3@yahoo.gr>
2020-02-26 21:28:01 +0200

    first stage of mysql parser is complete and ready for testing, moved from visual studio testingtools to NUNIT

--- Firedump/Firedump/Program.cs
+++ Firedump/Firedump/Program.cs
@@ -16,34 +16,8 @@ namespace Firedump
         [STAThread]
         static void Main(string[] args)
         {
-
-            string sql = @"Select * from table1; 
-                COMMIT;
-DELIMITER   $$   
-USE `moviecould`;
-CREATE DEFINER=`root`@`localhost` PROCEDURE `ptest3`()
-BEGIN
-
-	select * from fasts limit 3;
-    select * from fasts limit 3;
-END     $$      
-DELIMITER ;  
-;
-insert t set something;
-DELIM;";
-
-            SqlStatementParserWrapper parser = new SqlStatementParserWrapper(sql,sqlitetables.DbTypeEnum.MYSQL);
-            List<StatementRange> results = parser.Parse();
-            foreach(StatementRange p in results)
-            {
-                Console.WriteLine("--------SQL---------");
-                Console.WriteLine("START:" + (int)p.start);
-                Console.WriteLine("END:" + (int)p.end);
-                Console.WriteLine(sql.Substring((int)p.start, (int)p.end));
-                //Console.WriteLine(sql.Substring(40, 6));
-            }
-            //Application.EnableVisualStyles();
-            //Application.Run(new MainHome());
+            Application.EnableVisualStyles();
+            Application.Run(new MainHome());
         }
 
     }
