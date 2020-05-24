9d786d79363766d96dc9917128803da73d43b7c0
avraampiperidis <piperidis3@yahoo.gr>
2020-05-24 15:18:50 +0300

    gh-pages

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-## WIP
-###### Public Task tracker/progress on trello 
-<a target="_blank" href="https://trello.com/b/hoXLWMX2/lightbox">https://trello.com/b/hoXLWMX2/lightbox</a> <br>
-<b>Early pic.</b>
-<br>
-<img src="https://github.com/SqlBox/LightBox/blob/master/icons/pic.png?raw=true" width="1050" />
-<br>
+# lightbox homepage site
---
+++ js/data.js
@@ -0,0 +1,49 @@
+const PageData = {
+    logoImgUrl:'./assets/img/lightbox.png',
+    githubLogo:'./assets/icons/github.svg',
+    wip:'Work In Progress',
+    githubLogoAlt:'github logo',
+    repo:'https://github.com/SqlBox/LightBox',
+    title:'LightBox',
+    titleHeadline:'LightBox IDE',
+    titleDescription:'Open source Multi Database SQL client IDE. Focusing on simplicity, speed, small footprint',
+    appScreenShot:'./assets/img/introvid.gif',
+    appScreenShotAlt:'app screenshot',
+    vid1:'./assets/img/vid1.gif',
+    navItems:[
+        {title:'Overview'},
+        {title:'Downloads'},
+        {title:'Feautures'},
+        {title:'Docs'},
+        {title:'Apps'},
+    ],
+    footer: {
+        content:'Content',
+        items:[
+            {title:'Firedump',link:'https://github.com/SqlBox/firedump'},
+            {title:'Trask tracker',link:'https://trello.com/b/hoXLWMX2/lightbox'},
+            {title:'SqlStatementParser',link:'https://github.com/SqlBox/SqlStatementParser'}
+        ],
+        copyright:{
+            caption:'© 2020 Copyright:'
+        }
+    },
+    feautures:{
+        title:'Features',
+        items:[
+            {title:'Intellisense Editor',img:'./assets/img/vid1.gif',imgAlt:'Intellisense Editor'},
+            {title:'Lazy load limit results',img:'./assets/img/lazy.gif',imgAlt:'lazy load'},
+            {title:'Multi Database support',img:'./assets/img/choosedb.png',imgAlt:'multi database support'},
+        ]
+    },
+    downloads:{
+        title:'Downloads',
+        description:'Downloads are available for 32bit and 64bit executables.',
+        requirements:'Minimum requirement to run the app is to have .NET 4.6.1 and above',
+        links:[
+            {title:'DebugX86 exe',link:'https://github.com/SqlBox/LightBox/releases/download/debug64/debug64.zip'},
+            {title:'DebugX64 exe',link:'https://github.com/SqlBox/LightBox/releases/download/debug64/debug64.zip'}
+        ]
+    }
+}
+
