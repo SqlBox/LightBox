93d9e89906b2d6aacc2ad7acd034bf850e0939ad
avraampiperidis <piperidis3@yahoo.gr>
2020-05-28 23:01:11 +0300

    page update

--- js/data.js
+++ js/data.js
@@ -1,12 +1,14 @@
 const PageData = {
     logoImgUrl:'./assets/img/lightbox.png',
     githubLogo:'./assets/icons/github.svg',
+    isWip:true,
     wip:'Work In Progress',
     githubLogoAlt:'github logo',
     repo:'https://github.com/SqlBox/LightBox',
     title:'LightBox',
     titleHeadline:'LightBox IDE',
     titleDescription:'Open source Multi Database SQL client IDE. Focusing on simplicity, speed, small footprint',
+    secondDescription:'Lightbox is a light weight and robust frontend for databases. It offers a powerful text editor, a sql console, object browser and a few useful tools. It has small footprint.Lightbox is not comprehensive as MySql-Workbench or PLSQL Developer and never will provide so much functionality but some people like it clean and fast.',
     appScreenShot:'./assets/img/introvid.gif',
     appScreenShotAlt:'app screenshot',
     vid1:'./assets/img/vid1.gif',
@@ -39,7 +41,7 @@ const PageData = {
     downloads:{
         title:'Downloads',
         description:'Downloads are available for 32bit and 64bit executables.',
-        requirements:'Minimum requirement to run the app is to have .NET 4.6.1 and above',
+        requirements:'Minimum requirement to run the app is to have installed .NET 4.6.1 and above',
         links:[
             {title:'DebugX86 exe',link:'https://github.com/SqlBox/LightBox/releases/download/debug64/debug64.zip'},
             {title:'DebugX64 exe',link:'https://github.com/SqlBox/LightBox/releases/download/debug64/debug64.zip'}
