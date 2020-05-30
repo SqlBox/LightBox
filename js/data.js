0e9d0934baa1478b61b07e33ef1345303b0b618f
Avraam Piperidis <piperidis3@yahoo.gr>
2020-05-30 23:57:35 +0300

    first stage site for lightbox completed

--- js/data.js
+++ js/data.js
@@ -13,30 +13,48 @@ const PageData = {
     appScreenShotAlt:'app screenshot',
     vid1:'./assets/img/vid1.gif',
     navItems:[
-        {title:'Overview'},
-        {title:'Downloads'},
-        {title:'Feautures'},
-        {title:'Docs'},
-        {title:'Apps'},
+        {title:'Downloads',href:'#downloads'},
+        {title:'Feautures', href:'#features'},
+        {title:'Apps',href:'#apps'},
+        {title:'More',href:'#footer'}
     ],
     footer: {
         content:'Content',
         items:[
             {title:'Firedump',link:'https://github.com/SqlBox/firedump'},
-            {title:'Trask tracker',link:'https://trello.com/b/hoXLWMX2/lightbox'},
+            {title:'Task tracker',link:'https://trello.com/b/hoXLWMX2/lightbox'},
             {title:'SqlStatementParser',link:'https://github.com/SqlBox/SqlStatementParser'}
         ],
         copyright:{
             caption:'© 2020 Copyright:'
         }
     },
-    feautures:{
+    features:{
         title:'Features',
         items:[
-            {title:'Intellisense Editor',img:'./assets/img/vid1.gif',imgAlt:'Intellisense Editor'},
-            {title:'Lazy load limit results',img:'./assets/img/lazy.gif',imgAlt:'lazy load'},
-            {title:'Multi Database support',img:'./assets/img/choosedb.png',imgAlt:'multi database support'},
-        ]
+            {title:'Intellisense Editor',description:'Schema tables and fields plus common sql commands',img:'./assets/img/vid1.gif',imgAlt:'Intellisense Editor'},
+            {title:'Lazy load limit results',description:'Limit the results you want. Auto fast fetch next chunk while scrolling',img:'./assets/img/lazy.gif',imgAlt:'lazy load'},
+            {title:'Multi Database support',description:'Databases supported MYSQL, POSTGRESQL, SQLITE, MSSQL, ORACLE, MARIADB, IBM DB2, FIREBIRD',img:'./assets/img/choosedb.png',imgAlt:'multi database support'},
+        ],
+        extraFeatures:{
+            title:'Database vendor support progress',
+            featureProgress:[
+                {completed:true,title:'MySql'},
+                {completed:true,title:'Sqlite'},
+                {completed:true,title:'MariaDb'},
+                {completed:false,title:'PostgreSql'},
+                {completed:false,title:'MSSQL'},
+                {completed:false,title:'Oracle'},
+                {completed:false,title:'IBM Db2'},
+                {completed:false,title:'Firebird'}
+            ],
+            trello:{
+                title:'Progress',
+                icon:'./assets/icons/trello-logo-blue.svg',
+                url:'https://trello.com/b/hoXLWMX2/lightbox',
+                alt:'trello',
+            }
+        }
     },
     downloads:{
         title:'Downloads',
@@ -45,6 +63,13 @@ const PageData = {
         links:[
             {title:'DebugX86 exe',link:'https://github.com/SqlBox/LightBox/releases/download/debug64/debug64.zip'},
             {title:'DebugX64 exe',link:'https://github.com/SqlBox/LightBox/releases/download/debug64/debug64.zip'}
+        ]
+    },
+    apps:{
+        title:'Other apps and tools',
+        tools:[
+            {title:'Firedump',imgAlt:'firedump pic',img:'./assets/img/firedump.png',description:'MySql  backup...and even more .net desktop app',url:'https://github.com/SqlBox/firedump'},
+            {title:'SqlStatementParser',description:'A multy database sql statement parser in .net core',url:'https://github.com/SqlBox/SqlStatementParser'}
         ]
     }
 }
